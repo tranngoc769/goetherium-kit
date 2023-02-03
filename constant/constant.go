abbf1dc2cccc09235be832e6821f8256a68b2928
tranngoc769 <quang@ahamove.com>
2023-02-03 15:02:39 +0700

    constants

---
+++ constant/constant.go
@@ -0,0 +1,7 @@
+package constant
+
+const (
+	PrivateKey     = "65d1ced12a1bbd0d9fff6e18836dacfc110583a5d4038eff343388aeb524369d"
+	ChainUrl       = "https://rpc-mumbai.maticvigil.com/v1/4a5871d073741a11a25b589ad71e07ac6470c942"
+	AccountAddress = "0xF009a887E082918DAD73C1746BF04d3FA62991B6"
+)
